4c35a26c5958b2f562cf47d28a602b891c1d28de
Eduardo <eduardogomez.sent2@gmail.com>
2024-06-20 17:38:12 -0500

    fix: name correctly solidity2 page

--- src/data/pages.js
+++ src/data/pages.js
@@ -1,6 +1,6 @@
-import { Solidity } from "../pages";
+import { Solidity, Solidity2 } from "../pages";
 
 export const pages = [
     {name:'Solidity Sintaxis Part 1', route:'/solidity', component:Solidity, level:1},
-    {name:'Solidity Sintaxis Part 2', route:'/solidity2', component:Solidity, level:2},
+    {name:'Solidity Sintaxis Part 2', route:'/solidity2', component:Solidity2, level:2},
 ];
